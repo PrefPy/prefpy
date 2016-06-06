6ecd7d786496cfcb947ce5695e53d7fd26ee99ac
pdpiech <pdpiech@gmail.com>
2016-06-06 12:12:21 -0400

    Correct package setup for release as version 0.8.1

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup
 
 if __name__ == "__main__":
+
+    try:
+        import pypandoc
+        long_description = pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError):
+        long_description = open('README.md').read()
+
+    __version__ = "0.8.1"
+    base_url = "https://github.com/xialirong/prefpy"
+
     setup(name="prefpy",
-          version="0.8",
+          version=__version__,
           description="Rank aggregation algorithms",
+          long_description=long_description,
           classifiers=[
               "Development Status :: 3 - Alpha",
               "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
@@ -12,7 +23,8 @@ if __name__ == "__main__":
               "Intended Audience :: Science/Research",
               "Topic :: Scientific/Engineering"
           ],
-          url="http://github.com/xialirong/prefpy",
+          url=base_url,
+          download_url="{0}/archive/prefpy-{1}.tar.gz".format(base_url, __version__),
           author="Peter Piech",
           license="GPL-3",
           packages=["prefpy"],
